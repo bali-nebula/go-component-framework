5d60f0c5c8fbfc1be87bfb3cfb55bd0c5d667ebe
Derk Norton <derk.norton@gmail.com>
2023-03-06 12:35:25 -0700

    The notary code is now working.

--- documents/notary_test.go
+++ documents/notary_test.go
@@ -11,12 +11,12 @@
 package documents_test
 
 import (
-	fmt "fmt"
 	abs "github.com/bali-nebula/go-component-framework/abstractions"
 	age "github.com/bali-nebula/go-component-framework/agents"
 	bal "github.com/bali-nebula/go-component-framework/bali"
 	doc "github.com/bali-nebula/go-component-framework/documents"
 	ass "github.com/stretchr/testify/assert"
+	osx "os"
 	tes "testing"
 )
 
@@ -27,32 +27,32 @@ func TestNotary(t *tes.T) {
 	var notary = doc.Notary(module)
 
 	var certificateV1 = notary.GenerateKey()
-	fmt.Printf("certificate (v1): %v\n", string(bal.FormatDocument(certificateV1)))
+	osx.WriteFile("./examples/certificateV1.bali", bal.FormatDocument(certificateV1), 0600)
 
 	var citation = notary.GetCitation()
-	fmt.Printf("citation: %v\n", string(bal.FormatDocument(citation)))
+	osx.WriteFile("./examples/citation.bali", bal.FormatDocument(citation), 0600)
 
 	var attributes = bal.Catalog(`[:]`)
-	var type_ = bal.Component("/nebula/test/Document/v1.2.3")
+	var type_ = bal.Component("/nebula/examples/Document/v1.2.3")
 	var tag = bal.NewTag()
 	var version = bal.Version("v1.2")
 	var permissions = bal.Moniker("/nebula/permissions/public/v1")
 	var previous abs.CitationLike
 	var document = doc.Document(attributes, type_, tag, version, permissions, previous)
-	fmt.Printf("document: %v\n", string(bal.FormatDocument(document)))
+	osx.WriteFile("./examples/document.bali", bal.FormatDocument(document), 0600)
 	citation = notary.CiteDocument(document)
 	ass.True(t, notary.CitationMatches(citation, document))
 
 	var contract = notary.NotarizeDocument(document)
-	fmt.Printf("contract: %v\n", string(bal.FormatDocument(contract)))
+	osx.WriteFile("./examples/contract.bali", bal.FormatDocument(contract), 0600)
 	ass.True(t, notary.SignatureMatches(contract, certificateV1.GetDocument().(abs.CertificateLike)))
 
 	var certificateV2 = notary.RefreshKey()
-	fmt.Printf("certificate (v2): %v\n", string(bal.FormatDocument(certificateV2)))
+	osx.WriteFile("./examples/certificateV2.bali", bal.FormatDocument(certificateV2), 0600)
 
 	var salt = bal.Binary(64)
 	var credentials = notary.GenerateCredentials(salt)
-	fmt.Printf("credentials: %v\n", string(bal.FormatDocument(credentials)))
+	osx.WriteFile("./examples/credentials.bali", bal.FormatDocument(credentials), 0600)
 
 	notary.ForgetKey()
 }
